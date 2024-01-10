36aede89bae505b006c57dd68a6a5d4dcb3c61c4
Priyal PORWAL <26priyalmrbd@gmail.com>
2024-01-10 13:30:17 +0530

    Make SToryBrain more scalable

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,24 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
-    let example = Story(title: "You see a fork in the road.",
-                        choice1: "Take a left.",
-                        choice2: "Take a right.")
-    let stories = [
-        Story(title: "You see a fork in the road.",
-              choice1: "Take a left.",
-              choice2: "Take a right."),
-        Story(title: "You see a tiger.",
-              choice1: "Shout for help.",
-              choice2: "Play dead."),
-        Story(title: "You find a treasure chest.",
-              choice1: "Open it.",
-              choice2: "Check for traps."),
-    ]
 
-    var currentQuestion = 0
-    var nextQuestion = 0
+    var storyBrain = StoryBrain()
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,22 +23,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        guard currentQuestion < stories.count - 1 else { return }
-        let currentSelection = sender.currentTitle!
-        if currentSelection == stories[currentQuestion].choice1 {
-            currentQuestion += 1
-        } else {
-            currentQuestion += 2
-        }
-        updateUI()
+        storyBrain.updateStory(for: sender.currentTitle)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
     }
     
-    func updateUI() {
-        guard currentQuestion < stories.count - 1 else { return }
+    @objc func updateUI() {
+        guard let story = storyBrain.nextStory() else { return }
 
-        storyLabel.text = stories[currentQuestion].title
-        choice1Button.setTitle(stories[currentQuestion].choice1, for: .normal)
-        choice2Button.setTitle(stories[currentQuestion].choice2, for: .normal)
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
     }
 }
 
